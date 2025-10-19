a32216abba6438f969f4ba3bfcc65e525d79e017
mxtimu <matimujunior3@gmail.com>
2025-10-19 23:50:48 +0200

    added file, show full description of the books table without using describe

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,10 @@
-USE alx_book_store;
-SHOW COLUMNS FROM books;
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM information_schema.COLUMNS 
+WHERE TABLE_SCHEMA = 'alx_book_store' 
+AND TABLE_NAME = 'books';
